4a6ee7be0af3c5ca30d9b911721662150c613b28
shinokun <hypatia.tile02021@gmail.com>
2026-08-28 20:51:13 +0900

    chore: add nix deveshell with node and pnpm

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Next.js learning environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "Next.js shell environment"
+          '';
+        };
+      }
+    );
+}
